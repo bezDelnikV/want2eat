0d96ddcd5387e512b65c340a3241750c8913486f
Home <Home@DESKTOP-KA21DG9>
2020-03-13 03:48:38 +0200

    change evrymin to week

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->command('test')
-            ->everyMinute();
+            ->weekly();
     }
 
     /**
