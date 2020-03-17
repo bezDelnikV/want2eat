ae4139347fd41205739ba69b3ab8f8d8d701298b
Home <Home@DESKTOP-KA21DG9>
2020-03-17 13:31:45 +0200

    add google sarch,tag manager

--- resources/views/layouts/HeaderAndFooter.blade.php
+++ resources/views/layouts/HeaderAndFooter.blade.php
@@ -7,6 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Доставка їжі Рівне. Піца, суші та інші страви з ресторанів - Хочу Їсти</title>
 
+
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T3Q3SB2');</script>
+
+
     <link rel="icon" href="{{ asset('/img/favicon.png')}}" type="image/x-icon">
     <link href="{{ asset('/css/HeaderAndFooter.css')}}" rel="preload" as="style">
 
@@ -16,8 +24,14 @@
     <link href="{{ asset('/css/bootstrap-datetimepicker.min.css') }}" rel="stylesheet" media="print"
           onload="this.media='all'">
     <link href="{{ asset('/css/PopUpStyle.css') }}" rel="stylesheet">
+
+    <meta name="google-site-verification" content="T79fHtnSV9bXEqXZwzlrrAl5-kjF1LtLQzMBLaosl10"/>
 </head>
 <body>
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T3Q3SB2"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
 @include('pop-up.authorize')
 @include('pop-up.order')
 @include('pop-up.profile')
